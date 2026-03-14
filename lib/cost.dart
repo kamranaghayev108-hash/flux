54af702ec2ff67bbd47a7d88af708bd3fb3f3a3b
Kamran Aghayev <kamranaghayev108@gmail.com>
2026-03-14 21:56:43 +0400

    s

--- lib/cost.dart
+++ lib/cost.dart
@@ -7,7 +7,7 @@ class Cost extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.backgrounColors,
+      backgroundColor: AppColors.backgroundColor,
 
       body: Stack(
         children: [
